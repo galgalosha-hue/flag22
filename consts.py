f127a259b64fe4fd1d8c64095f950268c0f9c3c1
Esther <pisaq11@gmail.com>
2026-08-16 11:44:48 +0300

    first write in main, consts and game_field

---
+++ consts.py
@@ -0,0 +1,5 @@
+RUNNING_STATE = 1
+LOSE_STATE = 2
+WIN_STATE = 3
+
+KEYS = ["up", "down", "left", "right"]
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import pygame
+import consts
+import game_field
+import screen
+import soldier
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+'''state = {
+    "original_arrow": Screen.create_arrow(consts.ARROW_IMG),
+    "rotated_arrow": None,
+    "is_bubble_fired": False,
+    "bubbles_popping": [],
+    "turns_left_to_add_row": consts.NUM_OF_TURNS_TO_ADD_ROW,
+    "is_window_open": True,
+    "state": consts.RUNNING_STATE,
+    "bullet_bubble": None,
+    "bubble_direction": None,
+    "mouse_angle": None
+}'''
 
+state = {
+    "is_window_open": True,
+    "state": consts.RUNNING_STATE,
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+}
 
+def main():
+    pygame.init()
+    #game_field.create()
+    print(game_field.create())
+    while state["is_window_open"]:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        handle_user_events()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+        elif state["state"] != consts.RUNNING_STATE:
+            continue
+        if event.type == pygame.KEYDOWN:
+            key=pygame.key.name(event.key)
+            if key == pygame.K_RETURN:
+                handle_user_enter()
+            elif key not in consts.KEYS:
+                pass
+            else:
+                handle_user_button(key)
+
+def handle_user_button(key):
+    if key == pygame.K_LEFT:
+    elif key == pygame.K_RIGHT:
+    elif key == pygame.K_UP:
+    else:
