90ab11ab9fafcb9db30a7d02d16f7c33a5069bc2
netastern8-cmd <neta.stern8@gmail.com>
2026-08-16 14:51:17 +0300

    change

--- consts.py
+++ consts.py
@@ -1,5 +1,4 @@
 import random
-
 RUNNING_STATE = 1
 LOSE_STATE = 2
 WIN_STATE = 3
@@ -13,6 +12,10 @@ KEYS = ["up", "down", "left", "right"]
 BACKGROUND_COLOR = (00 ,33 ,00)
 DISPLAY_SIZE = (500, 250)
 GRASS_SIZE = (30, 10)
-GRASS_PLACMENT_X =  random.randrange(0, 470)
-GRASS_PLACMENT_Y = random.randrange(0, 240)
-GRASS_PLACMENT = (GRASS_PLACMENT_X, GRASS_PLACMENT_Y)
+
+GRASS_LIST = []
+for time in range(22):
+    GRASS_PLACMENT_X = random.randrange(0, 470)
+    GRASS_PLACMENT_Y = random.randrange(0, 240)
+    GRASS_PLACMENT = (GRASS_PLACMENT_X, GRASS_PLACMENT_Y)
+    GRASS_LIST.append(GRASS_PLACMENT)
--- screen.py
+++ screen.py
@@ -1,18 +1,26 @@
 import pygame
 import consts
+import random
 pygame.init()
 
 def display():
-    window = pygame.display.set_mode(DISPLAY_SIZE)
+    window = pygame.display.set_mode(consts.DISPLAY_SIZE)
     pygame.display.set_caption('flag')
     window.fill(consts.BACKGROUND_COLOR)
     pygame.display.flip()
     return window
+
+
 def draw_grass(window):
-    for bush in range(20):
-        grass = pygame.image.load('grass.png')
-        grass = pygame.transform.scale(grass, GRASS_SIZE)
-        window.blit(grass, GRASS_PLACMENT)
+    grass = pygame.image.load('grass.png')
+    grass = pygame.transform.scale(grass, consts.GRASS_SIZE)
+    for i in consts.GRASS_LIST:
+        window.blit(grass, consts.GRASS_PLACMENT)
     pygame.display.flip()
     return grass
 
+'''window = True
+while window:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            window = False'''
