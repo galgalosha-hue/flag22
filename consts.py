3082a6d47c669dcc75268223ae74261a62778ce2
netastern8-cmd <neta.stern8@gmail.com>
2026-08-16 14:55:15 +0300

    change

--- consts.py
+++ consts.py
@@ -13,9 +13,3 @@ BACKGROUND_COLOR = (00 ,33 ,00)
 DISPLAY_SIZE = (500, 250)
 GRASS_SIZE = (30, 10)
 
-GRASS_LIST = []
-for time in range(22):
-    GRASS_PLACMENT_X = random.randrange(0, 470)
-    GRASS_PLACMENT_Y = random.randrange(0, 240)
-    GRASS_PLACMENT = (GRASS_PLACMENT_X, GRASS_PLACMENT_Y)
-    GRASS_LIST.append(GRASS_PLACMENT)
