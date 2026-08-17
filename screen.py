8ad9aea704e5059eef87be3192ae1fbeaba3c5b1
netastern8-cmd <neta.stern8@gmail.com>
2026-08-17 15:21:21 +0300

    change

--- screen.py
+++ screen.py
@@ -86,5 +86,20 @@ def dark_mode(window):
     draw_night_soldier(window)
     return window
 
+def draw_lose_message():
+    draw_message(consts.LOSE_MESSAGE, consts.LOSE_FONT_SIZE,
+                 consts.LOSE_COLOR, consts.LOSE_LOCATION)
+
+
+def draw_win_message():
+    draw_message(consts.WIN_MESSAGE, consts.WIN_FONT_SIZE,
+                 consts.WIN_COLOR, consts.WIN_LOCATION)
+
+
+def draw_message(message, font_size, color, location, window):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    window.blit(text_img, location)
+
 
 
