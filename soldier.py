f38eb40f6d0c53c63e8bf4112a4b8c356515345a
Esther <pisaq11@gmail.com>
2026-08-17 09:56:49 +0300

    updating soldier

--- soldier.py
+++ soldier.py
@@ -31,7 +31,12 @@ def calc_body(game_field):
 def calc_legs():
     count = 0
     legs = []
-      return legs
+    for i in range(consts.NUM_OF_MINES):
+        for j in range(consts.NUM_OF_COLS):
+            count += 1
+            if game_field[i][j] == consts.SOLDIER and 6<=count<=8:
+                legs.append([i, j])
+    return legs
 
 def calc_whole_soldier(game_field):
     whole_soldier = []
